e739e3db72f3315752b8121a3caaeee32285d47d
yahedonist <yatopolskov@gmail.com>
2023-12-13 00:24:04 +0300

    Initial Commit

---
+++ todo_list_swiftUI/todo_list_swiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  todo_list_swiftUIApp.swift
+//  todo_list_swiftUI
+//
+//  Created by Nikita on 13.12.2023.
+//
+
+import SwiftUI
+
+@main
+struct todo_list_swiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
